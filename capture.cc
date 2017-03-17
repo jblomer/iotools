7498fbabfe04c6fe66316a4012ce7edb8fe74278
Jakob Blomer <jblomer@cern.ch>
2017-03-17 14:39:54 +0100

    basic connection between tracer and capturer

--- capture.cc
+++ capture.cc
@@ -2,8 +2,39 @@
  * Copyright CERN; jblomer@cern.ch
  */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <cassert>
+#include <cstdio>
+#include <string>
+
 #include "wire_format.h"
 
+using namespace std;
+
 int main(int argc, char **argv) {
+  string fanout{"iotrace.fanout"};
+
+  unlink(fanout.c_str());
+  int retval = mkfifo(fanout.c_str(), 0666);
+  assert(retval == 0);
+  int fd_fanout = open(fanout.c_str(), O_RDONLY);
+  assert(fd_fanout >= 0);
+
+  struct iotrace_frame frame;
+  while (read(fd_fanout, &frame, sizeof(frame)) == sizeof(frame)) {
+    switch (frame.op) {
+      case IOO_OPEN:
+        printf("file wass opened\n");
+        break;
+      default:
+        printf("unknown operation\n");
+    }
+  }
+
   return 0;
 }
