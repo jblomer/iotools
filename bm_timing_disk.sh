fe9d7974b1860d1928be7e8ac3946aabf6616dea
Jakob Blomer <jblomer@cern.ch>
2017-05-15 15:05:52 +0200

    adjust benchmark script

--- bm_timing_disk.sh
+++ bm_timing_disk.sh
@@ -10,7 +10,7 @@ for format in $(cat bm_formats); do
   dat_file="${PREFIX}.${format}"
   for i in $(seq 1 $NITER); do
     free && ./clear_page_cache && free
-    /bin/time -f "%e" -o result_single_timing ./lhcb_opendata -i $dat_file -r
+    /bin/time -f "%e" -o result_single_timing ./lhcb_opendata -i $dat_file
     result="$result $(cat result_single_timing)"
   done
   echo "$result" >> $OUTPUT
