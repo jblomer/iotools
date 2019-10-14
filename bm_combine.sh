06ada48bb9b9760e2254c9cf7430a79ae6be9f26
Jakob Blomer <jblomer@cern.ch>
2019-10-14 10:25:36 +0200

    update spreadsheet with enabled tree cache

--- bm_combine.sh
+++ bm_combine.sh
@@ -8,17 +8,19 @@ fi
 
 if [ "x$BM_RESULT_SET" != "x" ]; then
    for result in ${BM_RESULT_SET}*~*.txt; do
-     method="direct"
-     if echo $result | grep -q "+"; then
-       method=$(echo $result | cut -d~ -f1 | cut -d+ -f2)
+     if ! echo $result | grep -q +mmap; then
+       method="direct"
+       if echo $result | grep -q "+"; then
+         method=$(echo $result | cut -d~ -f1 | cut -d+ -f2)
+       fi
+       format_suffix=$(echo $result | cut -d~ -f2)
+       compression=$(echo $format_suffix | cut -d. -f1)
+       container=$(echo $format_suffix | cut -d. -f2)
+       format="$container+$method-$compression"
+       grep "^${BM_FIELD}" $result | awk -v format=$format \
+         '{ for(i=2; i<NF; i++) printf "%s",$i OFS; if(NF) printf "%s",$NF; printf ORS} BEGIN {printf "%s ", format}' \
+         >> $BM_OUTPUT
      fi
-     format_suffix=$(echo $result | cut -d~ -f2)
-     compression=$(echo $format_suffix | cut -d. -f1)
-     container=$(echo $format_suffix | cut -d. -f2)
-     format="$container+$method-$compression"
-     grep "^${BM_FIELD}" $result | awk -v format=$format \
-       '{ for(i=2; i<NF; i++) printf "%s",$i OFS; if(NF) printf "%s",$NF; printf ORS} BEGIN {printf "%s ", format}' \
-       >> $BM_OUTPUT
    done
 else
   for result in $@; do
