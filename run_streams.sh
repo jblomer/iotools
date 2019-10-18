c21aec35ad4fd5ea124417f44c797296342bb3dc
Jakob Blomer <jblomer@cern.ch>
2019-10-18 13:34:39 +0200

    fix streams runner

--- run_streams.sh
+++ run_streams.sh
@@ -6,10 +6,8 @@ fi
 
 for sample in lhcb cms h1X10; do
   for compression in none zstd; do
-    for format in ntuple root; do
-      for nstreams in 1 2 4 8 16 32 64; do
-        make $SELECT_DATA_ROOT result_read_ssd.${sample}+N${nstreams}~${compression}.${format}.txt
-      done
+    for nstreams in 1 2 4 8 16 32 64; do
+      make $SELECT_DATA_ROOT result_read_ssd.${sample}+N${nstreams}~${compression}.ntuple.txt
     done
   done
 done
