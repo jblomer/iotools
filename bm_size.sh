4fa7a5cfcd7dcf93e65028cc07aadddc1fba663e
Jakob Blomer <jblomer@cern.ch>
2021-01-20 13:34:03 +0100

    be more resilient when looking at file size of non-existing paths

--- bm_size.sh
+++ bm_size.sh
@@ -8,7 +8,7 @@ for format in $(cat bm_formats); do
   suffix=$(echo $format | cut -d- -f1)
   compression=$(echo $format | cut -d- -f2)
   path="${DATA_PREFIX}/${FILE_NAME}~${compression}.${suffix}"
-  if ! stat $path 2>/dev/null 2>&1
+  if ! stat $path >/dev/null 2>&1
   then
     echo "Warning: $path not available" >&2
     size=0
