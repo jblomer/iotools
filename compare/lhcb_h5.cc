4e697be06fee7b9f03362a4172365978b1cce4d0
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2021-11-22 22:44:57 +0100

    Migrate `gen_lhcb_h5` and `lhcb_h5` to h5hep

--- compare/lhcb_h5.cc
+++ compare/lhcb_h5.cc
@@ -1,13 +1,14 @@
-#include "util_h5.h"
+#include "lhcb_event.h"
 
-#include <unistd.h>
+#include <h5hep/h5hep.hxx>
 
 #include <cassert>
-#include <cstdio>
-#include <string>
 #include <chrono>
-#include <iostream>
+#include <cstdio>
 #include <future>
+#include <iostream>
+#include <string>
+#include <unistd.h>
 
 #include <TApplication.h>
 #include <TCanvas.h>
@@ -51,6 +52,33 @@ static void Usage(char *progname) {
   printf("Usage: %s -i <input HDF5 file> [-s]\n", progname);
 }
 
+/// __COLUMN_MODEL__ is defined via -D compiler option (see Makefile) 
+using Builder = h5hep::schema::SchemaBuilder<__COLUMN_MODEL__>;
+
+auto InitSchema() {
+  // h5hep allows omitting columns in the read schema; here we only specify the required columns
+  return Builder::MakeStructNode<LhcbEvent>("LhcbEvent", {
+      Builder::MakePrimitiveNode<double>("H1_PX", HOFFSET(LhcbEvent, H1_PX)),
+      Builder::MakePrimitiveNode<double>("H1_PY", HOFFSET(LhcbEvent, H1_PY)),
+      Builder::MakePrimitiveNode<double>("H1_PZ", HOFFSET(LhcbEvent, H1_PZ)),
+      Builder::MakePrimitiveNode<double>("H1_ProbK", HOFFSET(LhcbEvent, H1_ProbK)),
+      Builder::MakePrimitiveNode<double>("H1_ProbPi", HOFFSET(LhcbEvent, H1_ProbPi)),
+      Builder::MakePrimitiveNode<int>("H1_isMuon", HOFFSET(LhcbEvent, H1_isMuon)),
+      Builder::MakePrimitiveNode<double>("H2_PX", HOFFSET(LhcbEvent, H2_PX)),
+      Builder::MakePrimitiveNode<double>("H2_PY", HOFFSET(LhcbEvent, H2_PY)),
+      Builder::MakePrimitiveNode<double>("H2_PZ", HOFFSET(LhcbEvent, H2_PZ)),
+      Builder::MakePrimitiveNode<double>("H2_ProbK", HOFFSET(LhcbEvent, H2_ProbK)),
+      Builder::MakePrimitiveNode<double>("H2_ProbPi", HOFFSET(LhcbEvent, H2_ProbPi)),
+      Builder::MakePrimitiveNode<int>("H2_isMuon", HOFFSET(LhcbEvent, H2_isMuon)),
+      Builder::MakePrimitiveNode<double>("H3_PX", HOFFSET(LhcbEvent, H3_PX)),
+      Builder::MakePrimitiveNode<double>("H3_PY", HOFFSET(LhcbEvent, H3_PY)),
+      Builder::MakePrimitiveNode<double>("H3_PZ", HOFFSET(LhcbEvent, H3_PZ)),
+      Builder::MakePrimitiveNode<double>("H3_ProbK", HOFFSET(LhcbEvent, H3_ProbK)),
+      Builder::MakePrimitiveNode<double>("H3_ProbPi", HOFFSET(LhcbEvent, H3_ProbPi)),
+      Builder::MakePrimitiveNode<int>("H3_isMuon", HOFFSET(LhcbEvent, H3_isMuon)),
+    });
+}
+
 int main(int argc, char **argv) {
   std::string inputPath;
   bool show = false;
@@ -78,55 +106,51 @@ int main(int argc, char **argv) {
 
   auto ts_init = std::chrono::steady_clock::now();
 
-  H5Row h5row;
-  auto fileId = H5Fopen(inputPath.c_str(), H5P_DEFAULT, H5F_ACC_RDONLY);
-  assert(fileId >= 0);
-  auto setId = H5Dopen(fileId, "/DecayTree", H5P_DEFAULT);
-  assert(setId >= 0);
-  auto spaceId = H5Screate_simple(1, &H5Row::kDefaultDimension, NULL);
-  assert(spaceId >= 0);
-  auto memSpaceId = H5Screate(H5S_SCALAR);
-  assert(memSpaceId >= 0);
+  auto schema = InitSchema();
+  auto file = h5hep::H5File::Open(inputPath);
+  // Use the default RNTuple cluster size as the size for HDF5 chunk cache
+  std::static_pointer_cast<h5hep::H5File>(file)->SetCache(50 * 1000 * 1000);
+  auto reader = Builder::MakeReaderWriter(file, schema);
 
-  H5Row::DataSet row;
-  hsize_t count = 1;
+  auto num_chunks = reader->GetNChunks();
+  auto chunk = std::make_unique<LhcbEvent[]>(reader->GetWriteProperties().GetChunkSize());
 
   auto hMass = new TH1D("B_mass", "", 500, 5050, 5500);
   
   std::chrono::steady_clock::time_point ts_first = std::chrono::steady_clock::now();
-  for (hsize_t i = 0; i < H5Row::kDefaultDimension; ++i) {
-    if (i && (i % 100000 == 0))
-      printf("  ... processed %llu events\n", i);
+  size_t count = 0;
+  for (size_t chunkIdx = 0; chunkIdx < num_chunks; ++chunkIdx) {
+    printf("processed %lu k events\n", count / 1000);
 
-    auto retval = H5Sselect_hyperslab(spaceId, H5S_SELECT_SET, &i, NULL, &count, NULL);
-    assert(retval >= 0);
-    retval = H5Dread(setId, h5row.fTypeId, memSpaceId, spaceId, H5P_DEFAULT, &row);
-    assert(retval >= 0);
+    auto num_rows = reader->ReadChunk(chunkIdx, chunk.get());
+    for (size_t i = 0; i < num_rows; ++i) {
+      const auto &row = chunk[i];
+      if (row.H1_isMuon || row.H2_isMuon || row.H3_isMuon) {
+	continue;
+      }
 
-    if (row.h1_is_muon || row.h2_is_muon || row.h3_is_muon) {
-      continue;
+      constexpr double prob_k_cut = 0.5;
+      if (row.H1_ProbK < prob_k_cut) continue;
+      if (row.H2_ProbK < prob_k_cut) continue;
+      if (row.H3_ProbK < prob_k_cut) continue;
+
+      constexpr double prob_pi_cut = 0.5;
+      if (row.H1_ProbPi > prob_pi_cut) continue;
+      if (row.H2_ProbPi > prob_pi_cut) continue;
+      if (row.H3_ProbPi > prob_pi_cut) continue;
+
+      double b_px = row.H1_PX + row.H2_PX + row.H3_PX;
+      double b_py = row.H1_PY + row.H2_PY + row.H3_PY;
+      double b_pz = row.H1_PZ + row.H2_PZ + row.H3_PZ;
+      double b_p2 = GetP2(b_px, b_py, b_pz);
+      double k1_E = GetKE(row.H1_PX, row.H1_PY, row.H1_PZ);
+      double k2_E = GetKE(row.H2_PX, row.H2_PY, row.H2_PZ);
+      double k3_E = GetKE(row.H3_PX, row.H3_PY, row.H3_PZ);
+      double b_E = k1_E + k2_E + k3_E;
+      double b_mass = sqrt(b_E*b_E - b_p2);
+      hMass->Fill(b_mass);
     }
-
-    constexpr double prob_k_cut = 0.5;
-    if (row.h1_prob_k < prob_k_cut) continue;
-    if (row.h2_prob_k < prob_k_cut) continue;
-    if (row.h3_prob_k < prob_k_cut) continue;
-
-    constexpr double prob_pi_cut = 0.5;
-    if (row.h1_prob_pi > prob_pi_cut) continue;
-    if (row.h2_prob_pi > prob_pi_cut) continue;
-    if (row.h3_prob_pi > prob_pi_cut) continue;
-
-    double b_px = row.h1_px + row.h2_px + row.h3_px;
-    double b_py = row.h1_py + row.h2_py + row.h3_py;
-    double b_pz = row.h1_pz + row.h2_pz + row.h3_pz;
-    double b_p2 = GetP2(b_px, b_py, b_pz);
-    double k1_E = GetKE(row.h1_px, row.h1_py, row.h1_pz);
-    double k2_E = GetKE(row.h2_px, row.h2_py, row.h2_pz);
-    double k3_E = GetKE(row.h3_px, row.h3_py, row.h3_pz);
-    double b_E = k1_E + k2_E + k3_E;
-    double b_mass = sqrt(b_E*b_E - b_p2);
-    hMass->Fill(b_mass);
+    count += num_rows;
   }
   auto ts_end = std::chrono::steady_clock::now();
   auto runtime_init = std::chrono::duration_cast<std::chrono::microseconds>(ts_first - ts_init).count();
