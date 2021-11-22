8dd3983d9c889f2e027bf34ac40b496852a7a2a5
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2021-11-22 22:44:57 +0100

    Added Apache-Arrow utility functions

---
+++ compare/util_arrow.h
@@ -0,0 +1,88 @@
+/**
+ * Copyright CERN; j.lopez@cern.ch
+ */
+
+#ifndef UTIL_ARROW_H_
+#define UTIL_ARROW_H_
+
+#include <arrow/builder.h>
+#include <arrow/table.h>
+
+#include <functional>
+#include <memory>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+/// \brief Helper class to build a `arrow::Table` when a given row limit is
+/// reached. The constructed table is handed over to a user-specified function.
+///
+/// This class is templated over the array builder types, e.g. `UInt32Builder`.
+template <class... Ts>
+class ArrowTableBuilder {
+  using Callback_t = std::function<void(std::shared_ptr<arrow::Table>)>;
+
+  std::tuple<Ts...> arrayBuilder;
+  std::shared_ptr<arrow::Schema> schema;
+  size_t rowLimit, count = 0;
+  Callback_t doSomethingWithTable;
+
+  void MakeTable() {
+    std::vector<std::shared_ptr<arrow::Array>> v;
+
+    // FIXME: should check the value of `args.Finish().ok()`
+    std::apply([&v](Ts&... args) { ((v.push_back(*args.Finish())), ...); },
+	       arrayBuilder);
+
+    auto table = arrow::Table::Make(schema, v);
+    doSomethingWithTable(table);
+    count = 0;
+  }
+
+  template <std::size_t... Is>
+  void AppendImpl(const std::tuple<typename Ts::value_type...>& values,
+		  std::index_sequence<Is...>)
+  {
+    ((std::get<Is>(arrayBuilder).Append(std::get<Is>(values))), ...);
+  }
+
+public:
+  ArrowTableBuilder(std::shared_ptr<arrow::Schema> schema, size_t rowLimit, Callback_t cb)
+    : schema(schema), rowLimit(rowLimit), doSomethingWithTable(cb) {}
+  ~ArrowTableBuilder() { if (count) MakeTable(); }
+
+  size_t GetCount() const { return count; }
+
+  /// \brief Append the given values to the corresponding array builder, i.e.
+  /// i-th value in `values` shall be appended to `std::get<i>(arrayBuilder)`
+  void Append(const std::tuple<typename Ts::value_type...>& values) {
+    AppendImpl(values, std::index_sequence_for<Ts...>{});
+    if (++count == rowLimit)
+      MakeTable();
+  }
+};
+
+template <class... Ts>
+ArrowTableBuilder<Ts...>& operator<<(ArrowTableBuilder<Ts...> &builder,
+				     const std::tuple<typename Ts::value_type...>& values)
+{
+  builder.Append(values);
+  return builder;
+}
+
+/// \brief Return the indices in `schema` for the columns specified in `names`.
+static inline std::vector<int>
+GetColumnIndices(const std::shared_ptr<arrow::Schema> schema, const std::vector<std::string>& names)
+{
+  auto status = schema->CanReferenceFieldsByNames(names);
+  if (!status.ok())
+    throw std::runtime_error(status.message());
+
+  std::vector<int> ret;
+  for (auto &I : names)
+    ret.push_back(schema->GetFieldIndex(I));
+  return ret;
+}
+
+#endif // UTIL_ARROW_H_
