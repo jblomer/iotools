8e83aa8bfc49c818f59f2718397bed01e5790372
Jakob Blomer <jblomer@cern.ch>
2022-05-30 22:55:43 +0200

    adjust gen_atlas to updated RNTuple API

--- gen_atlas.cxx
+++ gen_atlas.cxx
@@ -15,6 +15,7 @@
 
 #include <cassert>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -82,7 +83,8 @@ int main(int argc, char **argv) {
    assert(f && ! f->IsZombie());
 
    // Get a unique pointer to an empty RNTuple model
-   auto model = RNTupleModel::Create();
+   auto model = RNTupleModel::CreateBare();
+   std::map<std::string, void *> fieldAdresses;
 
    // We create RNTuple fields based on the types found in the TTree
    // This simple approach only works for trees with simple branches and only one leaf per branch
@@ -100,41 +102,44 @@ int main(int argc, char **argv) {
       std::cout << "Convert leaf " << l->GetName() << " [" << l->GetTypeName() << "]"
                 << " --> " << "field " << field->GetName() << " [" << field->GetType() << "]" << std::endl;
 
+      auto fieldName = field->GetName();
+      auto typeName = field->GetType();
+
       if (typeid(*b) == typeid(TBranchSTL) || typeid(*b) == typeid(TBranchElement)) {
          // TODO: generic way of dealing with vector<T>
-         if (field->GetType() == "std::vector<bool>") {
+         if (typeName == "std::vector<bool>") {
             std::vector<bool> **v = new std::vector<bool> *();
             tree->SetBranchAddress(b->GetName(), v);
-            model->GetDefaultEntry()->CaptureValue(field->CaptureValue(*v));
-            model->GetFieldZero()->Attach(std::move(field));
-         } else if (field->GetType() == "std::vector<float>") {
+            fieldAdresses[fieldName] = *v;
+         } else if (typeName == "std::vector<float>") {
             std::vector<float> **v = new std::vector<float> *();
             tree->SetBranchAddress(b->GetName(), v);
-            model->GetDefaultEntry()->CaptureValue(field->CaptureValue(*v));
-            model->GetFieldZero()->Attach(std::move(field));
-         } else if (field->GetType() == "std::vector<std::int32_t>") {
+            fieldAdresses[fieldName] = *v;
+         } else if (typeName == "std::vector<std::int32_t>") {
             std::vector<std::int32_t> **v = new std::vector<std::int32_t> *();
             tree->SetBranchAddress(b->GetName(), v);
-            model->GetDefaultEntry()->CaptureValue(field->CaptureValue(*v));
-            model->GetFieldZero()->Attach(std::move(field));
-         } else if (field->GetType() == "std::vector<std::uint32_t>") {
+            fieldAdresses[fieldName] = *v;
+         } else if (typeName == "std::vector<std::uint32_t>") {
             std::vector<std::uint32_t> **v = new std::vector<std::uint32_t> *();
             tree->SetBranchAddress(b->GetName(), v);
-            model->GetDefaultEntry()->CaptureValue(field->CaptureValue(*v));
-            model->GetFieldZero()->Attach(std::move(field));
+            fieldAdresses[fieldName] = *v;
          } else {
-            std::cout << "Unhandled " << field->GetType() << std::endl;
+            std::cout << "Unhandled " << typeName << std::endl;
             assert(false);
          }
       } else {
-         // Hand over ownership of the field to the ntuple model.  This will also create a memory location attached
-         // to the model's default entry, that will be used to place the data supposed to be written
-         model->AddField(std::move(field));
-         // We connect the model's default entry's memory location for the new field to the branch, so that we can
-         // fill the ntuple with the data read from the TTree
-         void *fieldDataPtr = model->GetDefaultEntry()->GetValue(l->GetName()).GetRawPtr();
-         tree->SetBranchAddress(b->GetName(), fieldDataPtr);
+         fieldAdresses[fieldName] = field->GenerateValue().GetRawPtr();
       }
+
+      // Hand over ownership of the field to the ntuple model.  This will also create a memory location attached
+      // to the model's default entry, that will be used to place the data supposed to be written
+      model->AddField(std::move(field));
+   }
+   model->Freeze();
+
+   auto entry = model->CreateBareEntry();
+   for (const auto &f : fieldAdresses) {
+      entry->CaptureValueUnsafe(f.first, f.second);
    }
 
    // The new ntuple takes ownership of the model
@@ -148,7 +153,7 @@ int main(int argc, char **argv) {
      std::cout << "Processing " << nEntries << " entries" << std::endl;
      for (decltype(nEntries) i = 0; i < nEntries; ++i) {
         tree->GetEntry(i);
-        ntuple->Fill();
+        ntuple->Fill(*entry);
 
         if (i && i % 100000 == 0)
            std::cout << "Wrote " << i << " entries" << std::endl;
