f53d2adb2c3780ae20486b40cb63281bdad8894c
Jakob Blomer <jblomer@cern.ch>
2024-12-02 09:35:32 +0100

    update rules example

--- rules/06_update_from_base/Event_v3_LinkDef.h
+++ rules/06_update_from_base/Event_v3_LinkDef.h
@@ -3,9 +3,7 @@
 #pragma link C++ class Base+;
 #pragma link C++ class Event+;
 
-// Correct to not give a target??
-
 #pragma read sourceClass = "Event" source = "int fBadBaseName;" version = "[2]" targetClass = \
-   "Event" target = "" code = "{ newObj->fBetterDerivedName = onfile.fBadBaseName; }"
+   "Event" target = "fBetterDerivedName" code = "{ newObj->fBetterDerivedName = onfile.fBadBaseName; }"
 
 #endif
